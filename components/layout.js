07f522da72605021f28a37c8a7d4e80b9269e720
Henning Bertram <hbertram@gmail.com>
2021-02-05 16:25:55 +0100

    first

---
+++ components/layout.js
@@ -0,0 +1,50 @@
+import Head from "next/head";
+
+import Link from "next/link";
+
+const name = "Henning";
+export const siteTitle = "bertramonline.com";
+
+export default function Layout({ children }) {
+  return (
+    <div>
+      <Head>
+        <title>{siteTitle}</title>
+        <link rel="icon" href="/favicon.ico" />
+ 
+      </Head>
+      <div id="content">
+        <div id="header">
+          <h1>bertramonline.com</h1>
+          <p className="larger">
+            <a href="https://misc.bertramonline.com/writings/">writings</a>
+            &nbsp;|&nbsp;
+            <a href="https://photos.bertramonline.com/">photos</a>
+          </p>
+          <p className="smaller">
+            <a href="https://familyphotos.bertramonline.com/">family photos</a>
+            &nbsp;|&nbsp;
+            <a href="https://misc.bertramonline.com/photoblog/">
+              old family photos
+            </a>
+            &nbsp;|&nbsp;
+            <a
+              href="
+					/about"
+            >
+              about
+            </a>
+          </p>
+        </div>
+
+        {children}
+
+        <div id="footer">
+          <p>
+            This site is lovingly by and large handmade in Copenhagen, Denmark.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import "@fontsource/ibm-plex-sans/400.css" 
+import "@fontsource/ibm-plex-sans/700.css" 
+import "@fontsource/ibm-plex-mono/400.css" 
+import "@fontsource/ibm-plex-mono/700.css" 
+import '../styles/style.css'
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
